1b6480ff20cccb09e6565636eee2d300e5d346b2
Zuwaynib <zainabadeoje@gmail.com>
2023-06-11 15:09:36 +0100

    This is a basic C program

---
+++ intro_to_c/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	float y;
+	char z;
+
+	x = 4;
+	y = 0.5;
+	z = 'B';
+
+	printf("My age is %d\n", x);
+	printf("My tolerance level is %f\n", y);
+	printf("My average grade in school is %c", z);
+
+	return(0);
+}
+
