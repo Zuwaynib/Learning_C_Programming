37c94ff1b519ac3d8078a8365a9fd9a2ff7553ce
Zuwaynib <zainabadeoje@gmail.com>
2023-06-27 15:20:04 +0100

    For loop to count from 1-100

---
+++ README.md
@@ -0,0 +1,1 @@
+This will be a repository of everything I learn about C programming
---
+++ for_loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * programs prints from 1-100
+ */
+
+int main()
+{
+	int k;
+
+	for(int k = 1; k <= 100; k++)
+	{
+		printf("%d\n", k);
+	}
+
+	return (0);
+}
+
+
