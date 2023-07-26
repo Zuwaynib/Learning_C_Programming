c7f655262f8ea0914549a670da8302d09fb33e82
Zuwaynib <zainabadeoje@gmail.com>
2023-07-26 08:52:03 +0100

    Program that prints the square root of a number

---
+++ sqrt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+/* Program  to calculate the square root of a number using sqrt() function */
+int main()
+{
+    int result = sqrt(25); /* The sqrt function should be used with the math.h header file*/
+    printf("The square root of the number is %d\n", result);
+    /* Prints the square root of the number */
+    
+    return(0);
+}
