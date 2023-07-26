99922ce9d1c93d2320d8154a59e44014b240646c
Zuwaynib <zainabadeoje@gmail.com>
2023-07-26 08:58:11 +0100

    calculate the cube root of a number

---
+++ cbrt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+/* Program  to calculate the cube root of a number using cbrt() function */
+int main()
+{
+    int result = cbrt(27); /* The cbrt function should be used with the math.h header file*/
+    printf("The cube root of the number is %d\n", result);
+    /* Prints the cube root of the number */
+    
+    return(0);
+}
